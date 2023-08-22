2514fc469b249ce7136ef4614f5ccc71a2cecff7
Elena Rotari <elena.rotari@icloud.com>
2023-08-22 14:26:10 +0200

    some style to nav

--- app/about/page.js
+++ app/about/page.js
@@ -1,4 +1,18 @@
 //  data
+import {
+  FaHtml5,
+  FaCss3,
+  FaJs,
+  FaReact,
+  FaWordpress,
+  FaFigma,
+} from "react-icons/fa";
+import {
+  SiNextdotjs,
+  SiFramer,
+  SiAdobexd,
+  SiAdobephotoshop,
+} from "react-icons/si";
 const aboutData = [
   {
     title: "skills",
--- app/components/Nav.js
+++ app/components/Nav.js
@@ -9,10 +9,6 @@ import {
   HiEnvelope,
 } from "react-icons/hi2";
 
-// next
-import { useRouter } from "next/router";
-import Link from "next/link";
-
 // nav data
 export const navData = [
   { name: "home", path: "/", icon: <HiHome /> },
@@ -31,16 +27,35 @@ export const navData = [
   },
 ];
 
+// next
+import { useRouter } from "next/navigation";
+import Link from "next/link";
+
 const Nav = () => {
-  // const router = useRouter();
-  // const pathname = router.pathname;
+  const router = useRouter();
+  const { pathname } = router;
+
   return (
     <nav className="flex flex-col items-center xl:justify-center gap-y-4 fixed h-max bottom-0 mt-auto xl:right-[2%] z-50 top-0 w-full xl:w-16 xl:max-w-md xl:h-screen">
       <div className="flex w-full xl:flex-col items-center justify-between xl:justify-center gap-y-10 px-4 md:px-40 xl:px-0 h-[80px] xl:h-max py-8 bg-white/10 backdrop-blur-sm text-3xl xl:text-xl xl:rounded-full">
         {navData.map((item, index) => {
           return (
-            <Link href={item.path} key={index}>
-              {item.icon}
+            <Link
+              href={item.path}
+              key={index}
+              className={`${item.path === pathname && "text-accent"}
+              hover:text-accent transition-all duration-600 items-center flex relative group `}
+            >
+              <div className="absolute pr-14 right-0 hidden xl:group-hover:flex ">
+                <div className=" relative flex text-accent items-center p-[10px] ">
+                  <div className="text-[16px] leading-none font-semibold capitalize ">
+                    {item.name}
+                  </div>
+                  //triangle
+                  <div className="border-solid border-l-white border-l-8 border-y-transparent border-y-[6px] border-r-0 absolute -right-2"></div>
+                </div>
+              </div>
+              <div>{item.icon}</div>
             </Link>
           );
         })}
